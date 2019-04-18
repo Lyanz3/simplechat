81ad4a6f7e77f6b0fbe8d9d8e8b420e10a57a770
dcutting133 <dcutting@umich.edu>
2019-04-18 16:01:22 -0400

    now with input

--- client.py
+++ client.py
@@ -1,8 +1,5 @@
 import socket
 
-hostname, sld, tld, port = 'www', 'test', 'edu', 80
-target = '{}.{}.{}'.format(hostname, sld, tld)
-
 # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -10,10 +7,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # client.connect((target, port))
 client.connect(('127.0.0.1', 9999))
 
-# send some data (in this case a HTTP GET request)
-client.send(b'big dick msg')
-
-# receive the response data (4096 is recommended buffer size)
-response = client.recv(4096).decode()
-
-print(response)
+while 1 == 1:
+    msg = input('to server: ')
+    client.send(msg.encode())
+    response = client.recv(4096).decode()
+    print('from server: ', response)
--- server.py
+++ server.py
@@ -1,27 +1,30 @@
 import socket
-import threading
+from threading import *
 
 bind_ip = '127.0.0.1'
 bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
-server.listen(5)  # max backlog of connections
+server.listen(5)  # connection buffer of 5
 
 print('Listening on {}:{}'.format(bind_ip, bind_port))
 
+class client(Thread):
+    def __init__(self, socket, address):
+        Thread.__init__(self)
+        self.sock = socket
+        self.addr = address
+        self.start()
 
-def handle_client_connection(client_socket):
-    request = client_socket.recv(1024).decode()
-    print('Received {}'.format(request))
-    client_socket.send(b'bigger dik msg')
-    client_socket.close()
+    def run(self):
+        while 1 == 1: # while socket is alive just keep reusing it
+            request = self.sock.recv(1024).decode()
+            print('Recieved: {}'.format(request))
+            self.sock.send(b'hey bby whats poppin')
 
-while True:
+while 1 == 1: # run loop forever
     client_sock, address = server.accept()
     print('{}:{} has connected to the server!'.format(address[0], address[1]))
-    client_handler = threading.Thread(
-        target=handle_client_connection,
-        args=(client_sock,)  # without comma you'd get a... TypeError: handle_client_connection() argument after * must be a sequence, not _socketobject
-    )
-    client_handler.start()
+    client(client_sock, address)
+    #client_sock.close()
