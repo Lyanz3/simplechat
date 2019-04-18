ff8f9e26a350c3a7a12b54577902cc00dd0830e6
dcutting133 <dcutting@umich.edu>
2019-04-18 17:44:21 -0400

    hi shawn

--- client.py
+++ client.py
@@ -6,9 +6,15 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect the client
 # client.connect((target, port))
 client.connect(('127.0.0.1', 9999))
+response = client.recv(4096).decode() # check for initial messages
+print('from server: ', response)
 
 while 1 == 1:
     msg = input('to server: ')
     client.send(msg.encode())
+
     response = client.recv(4096).decode()
+    sender, reciever, command, payload = response.split(',', 3)
     print('from server: ', response)
+
+# hi shawn
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 import socket
 from threading import *
 
-bind_ip = '127.0.0.1'
+bind_ip = '127.0.0.1' #127.0.0.1:9999
 bind_port = 9999
 
+#socket server;
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
 server.listen(5)  # connection buffer of 5
@@ -18,10 +19,12 @@ class client(Thread):
         self.start()
 
     def run(self):
+        self.sock.send('You are {}'.format(self.addr[1]).encode())
         while 1 == 1: # while socket is alive just keep reusing it
-            request = self.sock.recv(1024).decode()
-            print('Recieved: {}'.format(request))
-            self.sock.send(b'hey bby whats poppin')
+            request = self.sock.recv(1024)
+            print('{}: {}'.format(self.addr[1], request.decode()))
+            response = '{},{},{},{}'.format(0, self.addr[1], 0, 'message,,,,,,,,,,,,,here')
+            self.sock.send(response.encode())
 
 while 1 == 1: # run loop forever
     client_sock, address = server.accept()
