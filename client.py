b4ece867df968d8213071822f9cdf578443b2e38
dcutting133 <dcutting@umich.edu>
2019-04-19 16:21:16 -0400

    fixed broken part, now to beef up server

--- client.py
+++ client.py
@@ -4,69 +4,69 @@ from threading import Thread
 from command import Command
 from message import Message
 
-# flag to shutdown client
-quit = False
-# queues for keeping track of messages
-sendable_queue = Queue()
-recieved_queue = Queue()
+class Client():
+    client_id = 0 # the id given to the client by the Server
+    quit = False # flag to shutdown client
+    sendable_queue = Queue() # queues for keeping track of messages
+    recieved_queue = Queue()
 
 class Connection(Thread):
-    __init__(self, socket, address):
+    def __init__(self, socket):
         Thread.__init__(self)
         self.sock = socket
-        self.addr = address
+        self.start()
 
     def send(self):
-        sending_message = sendable_queue.get() # get the next sendable message from the queue
+        sending_message = Client.sendable_queue.get() # get the next sendable message from the queue
         self.sock.send(sending_message.sendable().encode()) # encode and send
 
     def recieve(self):
         response = self.sock.recv(4096).decode() # response from server
         recieved_message = Message() # create a message object to store
         recieved_message.parse(response) # parse message into usable format
-        recieved_queue.put(recieved_message) # add the message to the message queue
+        Client.recieved_queue.put(recieved_message) # add the message to the message queue
 
     def run(self):
-        while not quit:
+        while not Client.quit:
             self.send()
             self.recieve()
 
-class Client(Thread, id):
-    __init__(self):
+class Interface(Thread):
+    def __init__(self):
         Thread.__init__(self)
-        self.id = id
+        self.start()
 
-    def update():
-        message = recieved_queue.get()
+    def update(self):
+        message = Client.recieved_queue.get()
         print('[{}]: {}'.format(message.sender, message.content))
 
     def run(self):
-        while not quit:
+        while not Client.quit:
             self.update()
             user_input = input('-> ')
-            message = Message(id,,, user_input)
+            message = Message(Client.client_id, 0, Command(1), user_input)
             if user_input == '/leave':
                 message.command = Command(4) # leave command
-                quit = True
-            sendable_queue.put(message)
+                Client.quit = True
+            Client.sendable_queue.put(message)
 
 if  __name__ =='__main__':
     # create an ipv4 socket object using the tcp
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 9999))
     init_response = client_socket.recv(4096).decode() # check for initial messages
-    client_id = int(init_response) # first message sent from server after connect is id
+    Client.client_id = int(init_response) # first message sent from server after connect is id
+    print('Connected to server as [{}]'.format(Client.client_id))
 
-    client = Client(client_id)
-    client.setName('User Interface')
+    interface = Interface()
+    interface.setName('User Interface')
 
-    connection = Connection(client_socket, client_socket.address)
+    connection = Connection(client_socket)
     connection.setName('Server Connection')
 
-    client.start()
-    connection.start()
-
-    client.join()
+    interface.join() # dont close until all threads are done
     connection.join()
+
+    client_socket.close()
 
     print('Exiting...')
--- server.py
+++ server.py
@@ -1,46 +1,48 @@
 import socket
 from threading import *
-from message import message
-from command import command
+from message import Message
+from command import Command
 import queue
 
-bind_ip = '127.0.0.1' #127.0.0.1:9999
-bind_port = 9999
-message_queue = queue.Queue()
+class Server():
+    bind_ip = '127.0.0.1' #127.0.0.1:9999
+    bind_port = 9999
+    message_queue = queue.Queue()
+    quit = False
 
-#socket server;
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((bind_ip, bind_port))
-server.listen(5)  # connection buffer of 5
-
-print('Listening on {}:{}'.format(bind_ip, bind_port))
-
-class connection(Thread):
+class Connection(Thread):
     def __init__(self, socket, address):
         Thread.__init__(self)
         self.sock = socket
         self.addr = address
+        self.sock.send('{}'.format(self.addr[1]).encode()) # send id to client
+        print('{}:{} has connected to the server!'.format(address[0], address[1]))
         self.start()
 
+    def send(self):
+        for m in Server.message_queue.queue:
+            if m.sender == self.addr[1] or m.sender == 0:
+                self.sock.send(m.sendable().encode())
+
+    def recieve(self):
+        request = self.sock.recv(1024).decode()
+        recieved_message = Message() # create a message object to store
+        recieved_message.parse(request) # parse message into usable format
+        Server.message_queue.put(recieved_message) # put the message into the queue
+
     def run(self):
-        self.sock.send('You are {}'.format(self.addr[1]).encode())
-        while 1 == 1: # while socket is alive just keep reusing it
-            request = self.sock.recv(1024).decode()
-            recieved_message = message() # create a message object to store
-            recieved_message.parse(request) # parse message into usable format
-            message_queue.put(recieved_message) # put the message into the queue
-            print('recieved message')
+        while not Server.quit: # while socket is alive just keep reusing it
+            self.send()
+            self.recieve()
 
-            message_queue.join() # send all messages that are listed to host
-            for m in message_queue:
-                if m.sender == self.addr[1] or m.sender == 0:
-                    self.sock.send(m.sendable().encode())
-            message_queue.task_done() # mutex for thread safety
+if  __name__ =='__main__':
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((Server.bind_ip, Server.bind_port))
+    server_socket.listen(5)  # connection buffer of 5
+    print('Listening on {}:{}'.format(Server.bind_ip, Server.bind_port))
+    client_socket, address = server_socket.accept()
 
-while 1 == 1: # run loop forever
-    client_sock, address = server.accept()
-    print('{}:{} has connected to the server!'.format(address[0], address[1]))
-    connection(client_sock, address)
-    if message_queue.qsize() > 100: # limit message queue to 100 messages
-        message_queue.pop()
-    #client_sock.close()
+    connection = Connection(client_socket, address)
+    connection.join()
+
+    server_socket.close()
